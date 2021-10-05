831de4fdb33a9e52b7b1e90fcf4f5fb00ee5652a
FilmyFather <91774433+FilmyFather@users.noreply.github.com>
2021-10-05 12:15:56 +0530

    Update script.py

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ I'm A simple Zee5 link downloader bot With Permanent Thumbnail Support💯.
 
 Please send me any Zee5 link, I can upload it to telegram as File/Video.
 
-Currently I'm in beta mode 🥺, If found any bugs, report @TroJanzSupport !
+Currently I'm in beta mode 🥺, If found any bugs, report @RequestingHuB !
 
 Click <i>/help</i> for more details....</b>"""
 
@@ -21,9 +21,9 @@ Click <i>/help</i> for more details....</b>"""
 NOTE: Download may take some time! So please wait for it to complete!"""
 
 
-    ABOUT_TEXT = """⭕️<b>My Name : TroJanZ Zee5 DL</b>
+    ABOUT_TEXT = """⭕️<b>My Name : FilmyFather Zee5 DL</b>
 
-⭕️<b>Creater :</b> @TroJanzHEX
+⭕️<b>Creater :</b> @Yuvi_4502
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
@@ -59,7 +59,7 @@ NOTE: Download may take some time! So please wait for it to complete!"""
     
     NO_THUMB = "SED😕 No saved thumbnails Found!!"
     
-    CUSTOM_CAPTION_UL_FILE = "<b>{newname}\n\n©️ @TroJanzHEX</b>"
+    CUSTOM_CAPTION_UL_FILE = "<b>{newname}\n\n©️ @FilmyFather_Latest</b>"
     
     TIMEOUT = "<b><i>Sorry for the delay. It'll help reduce the flood wait</i> 😇\n\nWait for {} sec and try again.</b>"
     
